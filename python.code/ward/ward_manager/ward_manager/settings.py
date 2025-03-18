1af1f83a79dcf02112a42418071038d06ca35f81
homedesk <holloway75@126.com>
2025-03-19 00:24:22 +0800

    3.19

--- python.code/ward/ward_manager/ward_manager/settings.py
+++ python.code/ward/ward_manager/ward_manager/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -52,10 +51,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ward_manager.urls'
 
+
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
