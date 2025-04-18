c65b747a164051d9141be107d39f17c228180d44
holloway <holloway75@.com>
2025-04-18 19:26:42 +0800

    4.18

--- python.code/pythonProject/main.py
+++ python.code/pythonProject/main.py
@@ -1,11 +1,12 @@
 import os
-
+from maxdense import MaxDense
 import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
     os.chdir('E:\我的坚果云\ECS_final')
     df = pd.read_excel('variants.xlsx', index_col=0)
-    a1 = np.array((1,2,3))
-    a2 = np.array((2,3,4))
-    print(a1 * a2)
+    md = MaxDense(df)
+
+
+
--- python.code/pythonProject/maxdense.py
+++ python.code/pythonProject/maxdense.py
@@ -1,9 +1,10 @@
-import datetime
+from unittest.mock import inplace
 
 import pandas as pd
 import numpy as np
-from statsmodels.sandbox.distributions.genpareto import shape
-from statsmodels.sandbox.stats.multicomp import TukeyHSDResults
+import os
+
+from fontTools.merge.util import first
 
 
 def prob_merge(arr_:np.ndarray):
@@ -17,31 +18,53 @@ class Preprocessor:
         :param df: 'af', 'gene', 'inheritance' were required in columns.
         :param ban_variants_list:
         """
-        df.sort_values(by='gene', inplace=True)
         if ban_variants_list:
             df.drop(ban_variants_list, inplace=True)
+        df.sort_values(by='af', ascending=False, inplace=True)
+        self.gene_list = list(set(df['gene']))
 
-        columns = ['af', 'cont', 'dense', 'gene']
-        self.data = pd.DataFrame(columns=columns, index=df.index)
-        self.data[['af', 'gene']] = df[['af', 'gene']]
+        columns = ['af', 'cont', 'dense', 'gene', 'id']
+        self.data = pd.DataFrame(columns=columns)
+        self.data = self.data.astype({'af':'float64', 'cont':'float64', 'dense':'float64', 'gene':'object', 'id':'object'})
 
-
-    def get_cont(self, dict_gene_risk):
-        pass
-
-
-
-
+        self.data[['af', 'gene', 'id']] = df[['af', 'gene', 'id']]
+        second_level = self.data.groupby('gene').cumcount()
+        self.data.set_index(['gene', second_level], inplace=True)
 
 
 class MaxDense(Preprocessor):
     def __init__(self, df, ban_variants_list=None):
         super().__init__(df=df, ban_variants_list=ban_variants_list)
-        self.dict_cont_perct = dict()
+        self.get_cont()
+
+    def get_cont(self):
+        auto_list, xlink_list = self._get_inherit_type_list()
+        for gene in self.gene_list:
 
 
-    def _get_cont(self, gene):
-        pass
 
-    def get_dense(self):
-        pass
+            afs = np.array(self.data.xs(gene)['af'])
+            var_counts = len(afs)
+            merged_afs = np.array([0] + [prob_merge(afs[:i+1]) for i in range(var_counts)])
+            if gene in auto_list:
+                merged_afs = merged_afs ** 2
+            elif gene in xlink_list:
+                pass
+            else:
+                raise ValueError()
+
+            self.data.loc[gene, 'cont'] = [merged_afs[i+1] - merged_afs[i] for i in range(var_counts)]
+
+            print(self.data.loc[gene].dtypes)
+            print(self.data.loc[gene])
+            exit()
+
+
+    @staticmethod
+    def _get_inherit_type_list():
+        os.chdir('E:\我的坚果云\ECS_mid')
+        with open('gene.autosome.list', 'r') as f:
+            auto_list = f.read().splitlines()
+        with open('gene.x_link.list', 'r') as f:
+            xlink_list = f.read().splitlines()
+        return auto_list, xlink_list
