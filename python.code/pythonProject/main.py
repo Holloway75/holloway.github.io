946724c67c93ad162c413a9dadff2632a7242a74
holloway <holloway75@.com>
2025-04-21 18:15:54 +0800

    4.21

--- python.code/pythonProject/main.py
+++ python.code/pythonProject/main.py
@@ -1,12 +1,14 @@
 import os
 from maxdense import MaxDense
-import numpy as np
 import pandas as pd
 
 if __name__ == '__main__':
     os.chdir('E:\我的坚果云\ECS_final')
     df = pd.read_excel('variants.xlsx', index_col=0)
     md = MaxDense(df)
+
+    md.form_chain(100)
+    print(md.result['counts'].max())
 
 
 
--- python.code/pythonProject/maxdense.py
+++ python.code/pythonProject/maxdense.py
@@ -25,14 +25,26 @@ class Preprocessor:
 class MaxDense(Preprocessor):
     def __init__(self, df, ban_variants_list=None):
         super().__init__(df=df, ban_variants_list=ban_variants_list)
+        self.remain_loci = None
+        self.panel_size = None
+        self.result_genes = []
+        self.result_varid = []
+        self.result_counts = []
+        self.result = None
+
         gene_list = list(set(df['gene']))
-        record_list = [[0, self.data.loc[gene].shape[0]] for gene in gene_list]
-        self.records = dict(zip(gene_list, record_list))
+        record = {
+            'starts': np.zeros(len(gene_list)),
+            'var_counts': [self.data.loc[gene].shape[0] for gene in gene_list]
+        }
+        self.records = pd.DataFrame(data=record, index=gene_list)
+        self.records = self.records.astype({'starts': 'int32', 'var_counts': 'int32'})
         self.get_cont()
+
 
     def get_cont(self):
         auto_list, xlink_list = self._get_inherit_type_list()
-        for gene,[_,var_counts] in self.records.items():
+        for gene, var_counts in zip(self.records.index, self.records['var_counts']):
             afs = np.array(self.data.xs(gene)['af'])
             merged_afs = 2 * np.array([0] + [prob_merge(afs[:i+1]) for i in range(var_counts)])
             if gene in auto_list:
@@ -44,10 +56,6 @@ class MaxDense(Preprocessor):
 
             self.data.loc[gene, 'cont'] = [merged_afs[i+1] - merged_afs[i] for i in range(var_counts)]
             self.data.loc[gene, 'dense'] = self._get_dense(gene, 0, var_counts)
-            print(var_counts)
-
-            print(self.data.loc[gene])
-            exit()
 
 
     @staticmethod
@@ -65,3 +73,48 @@ class MaxDense(Preprocessor):
 
 
     def form_chain(self, panel_size):
+        self.remain_loci = panel_size
+        while self.remain_loci:
+            self._tail_removal()
+            self._prolong_chain()
+
+        data = {
+            'gene': self.result_genes,
+            'varid': self.result_varid,
+            'counts':self.result_counts
+        }
+        self.result = pd.DataFrame(data=data)
+
+    def _tail_removal(self):
+        condition = self.records['var_counts'] > self.remain_loci
+        _indices = self.records[condition].index
+
+        if len(_indices) == 0:
+            pass
+        else:
+            for gene in _indices:
+                _start = self.records.loc[gene, 'starts'] + self.remain_loci
+                _end = self.records.loc[gene, 'starts'] + self.records.loc[gene, 'var_counts']
+                col_data = self.data.loc[gene, 'dense'].values
+                col_data[_start: _end] = 0
+                self.data.loc[gene, 'dense'] = col_data
+                self.records.loc[gene, 'var_counts'] = self.remain_loci
+
+
+    def _prolong_chain(self):
+        max_id = self.data['dense'].idxmax()
+        gene = max_id[0]
+        _start = self.records.loc[gene, 'starts']
+        _var_counts = max_id[1] - _start + 1
+        _end = _start + _var_counts
+
+        self.records.loc[gene, 'starts'] += _var_counts
+        self.result_genes.append(gene)
+        self.result_varid.append(max_id[1])
+        self.result_counts.append(_var_counts)
+        self.remain_loci -= _var_counts
+
+        col_data = self.data.loc[gene, 'dense'].values
+        col_data[_start: _end] = 0
+        self.data.loc[gene, 'dense'] = col_data
+
